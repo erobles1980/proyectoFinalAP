91cafac449bc7426a1ef7bffba89c31d8f4f612c
erobles1980 <eroblesp@est.ups.edu.ec>
2023-12-23 13:17:17 -0500

    USI: When I call the findAll method and it has no data, then it should throw an exception TEST-ID: USI_CA2 TYPE: feat RESULT: fail DESCRIPTION: Expected com.tdd.msa.user.usermsatdd.service.impl.EmptyListException to be thrown, but nothing was thrown

--- src/test/java/com/tdd/msa/user/usermsatdd/service/UserServiceImplTest.java
+++ src/test/java/com/tdd/msa/user/usermsatdd/service/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.tdd.msa.user.usermsatdd.service;
 
 import com.tdd.msa.user.usermsatdd.domain.UserEntity;
 import com.tdd.msa.user.usermsatdd.repository.UserRepository;
+import com.tdd.msa.user.usermsatdd.service.impl.EmptyListException;
 import com.tdd.msa.user.usermsatdd.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,12 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,11 +41,24 @@ class UserServiceImplTest {
         //@Given - setear los parametros de entrada segun lo requiera
 
         //@When - llama al metodo o simulamos los llamados
-        when(userServiceImplMock.findAll()).thenReturn(expectedUserEntites);
+        when(userRepository.findAll()).thenReturn(expectedUserEntites);
         List<UserEntity> userEntities=userServiceImplMock.findAll();
 
         //@Then - son las aserciones que validen las respuesta de lo que se devuelva
         assertNotNull(userEntities,"This object should not be null");
         assertEquals(expectedUserEntites.get(0).getId(),userEntities.get(0).getId());
+    }
+
+    @Test
+    @DisplayName("USI_CA2: When I call the findAll method and it has no data, then it should throw an exception")
+    void whenCallFindAllMethodAndItHasNoDataThenShouldThrowException(){
+        //@Given
+
+        //@When
+        when(userRepository.findAll()).thenReturn(List.of());
+
+        //@Then
+        //assertFalse(userEntities.isEmpty());
+        assertThrows(EmptyListException.class, () -> userServiceImplMock.findAll());
     }
 }
