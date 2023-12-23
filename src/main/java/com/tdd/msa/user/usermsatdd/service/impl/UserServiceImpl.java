45d05a9c50d46fd60b867a63383ab67b0c0062bd
erobles1980 <eroblesp@est.ups.edu.ec>
2023-12-23 11:28:23 -0500

    USI: When call findAll method then return lost of Users TEST-IS: USI_CAL TYPE: fix RESULT: pass

--- src/main/java/com/tdd/msa/user/usermsatdd/service/impl/UserServiceImpl.java
+++ src/main/java/com/tdd/msa/user/usermsatdd/service/impl/UserServiceImpl.java
@@ -1,15 +1,21 @@
 package com.tdd.msa.user.usermsatdd.service.impl;
 
 import com.tdd.msa.user.usermsatdd.domain.UserEntity;
+import com.tdd.msa.user.usermsatdd.repository.UserRepository;
 import com.tdd.msa.user.usermsatdd.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public List<UserEntity> findAll() {
-        return null;
+        List<UserEntity> userEntities=userRepository.findAll();
+        return userEntities;
     }
 }
--- src/test/java/com/tdd/msa/user/usermsatdd/service/UserServiceImplTest.java
+++ src/test/java/com/tdd/msa/user/usermsatdd/service/UserServiceImplTest.java
@@ -1,24 +1,31 @@
 package com.tdd.msa.user.usermsatdd.service;
 
 import com.tdd.msa.user.usermsatdd.domain.UserEntity;
+import com.tdd.msa.user.usermsatdd.repository.UserRepository;
 import com.tdd.msa.user.usermsatdd.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl userServiceImplMock;
+
+    @Mock
+    private UserRepository userRepository;
 
     private List<UserEntity> expectedUserEntites;
 
@@ -35,9 +42,11 @@ public class UserServiceImplTest {
         //@Given - setear los parametros de entrada segun lo requiera
 
         //@When - llama al metodo o simulamos los llamados
+        when(userServiceImplMock.findAll()).thenReturn(expectedUserEntites);
         List<UserEntity> userEntities=userServiceImplMock.findAll();
 
-        //@Then - son las acerciones que validen las respuesta de lo que se devuelva
+        //@Then - son las aserciones que validen las respuesta de lo que se devuelva
         assertNotNull(userEntities,"This object should not be null");
+        assertEquals(expectedUserEntites.get(0).getId(),userEntities.get(0).getId());
     }
 }
