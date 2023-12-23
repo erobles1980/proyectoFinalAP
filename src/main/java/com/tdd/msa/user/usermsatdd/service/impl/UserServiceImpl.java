92b79b11e7c3d98da867a403f0ddb3c6340f61c0
erobles1980 <eroblesp@est.ups.edu.ec>
2023-12-23 11:41:49 -0500

    USI: When call findAll method then return lost of Users TEST-IS: USI_CAL TYPE: refactor RESULT: pass

--- src/main/java/com/tdd/msa/user/usermsatdd/service/impl/UserServiceImpl.java
+++ src/main/java/com/tdd/msa/user/usermsatdd/service/impl/UserServiceImpl.java
@@ -3,19 +3,19 @@ package com.tdd.msa.user.usermsatdd.service.impl;
 import com.tdd.msa.user.usermsatdd.domain.UserEntity;
 import com.tdd.msa.user.usermsatdd.repository.UserRepository;
 import com.tdd.msa.user.usermsatdd.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
 
     @Override
     public List<UserEntity> findAll() {
-        List<UserEntity> userEntities=userRepository.findAll();
-        return userEntities;
+        return userRepository.findAll();
     }
 }
